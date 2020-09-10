b007672a1849a8ac62cfa1c023cd9a9ed18d0127
homeuser <zowie_i@mail.ru>
2020-09-10 18:15:39 +0300

    All Together with Maven: clean, compile, exec:java, failsafe:integration-test

---
+++ BookJavaee7/src/test/java/ru/unclediga/cdi/book/bookservice/BookServiceIT.java
@@ -0,0 +1,19 @@
+package ru.unclediga.cdi.book.bookservice;
+
+import org.jboss.weld.environment.se.Weld;
+import org.jboss.weld.environment.se.WeldContainer;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class BookServiceIT {
+    @Test
+    public void shouldCheckNumberIsMOCK() {
+        Weld weld = new Weld();
+        WeldContainer container = weld.initialize();
+        BookService bookService = container.instance().select(BookService.class).get();
+        Book book = bookService.createBook("H2G2", 12.5f, "Geeky scifi Book");
+        assertTrue(book.getNumber().startsWith("Mock"));
+        weld.shutdown();
+    }
+}
